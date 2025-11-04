a1209f026670d8ea9e87c5047caeb21f8c438352
Joel Ndiba Mwaura <jmwaura@byupathway.edu>
2025-11-04 05:43:35 +0300

    commit

---
+++ JoelNdiba-mvc-movies/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace JoelNdiba_mvc_movies.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
