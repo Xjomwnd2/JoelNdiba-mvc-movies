df0dd8f3bce520bcbacf4ebc36696e3c5d15d8af
Joel Ndiba Mwaura <jmwaura@byupathway.edu>
2025-11-04 08:04:58 +0300

    COMMIT

--- JoelNdiba-mvc-movies/Controllers/MoviesController.cs
+++ JoelNdiba-mvc-movies/Controllers/MoviesController.cs
@@ -2,7 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using JoelNdiba_mvc_movies.Data;
-using MvcMovie.Models;
+using JoelNdiba_mvc_movies.Models;
 
 namespace MvcMovie.Controllers
 {
